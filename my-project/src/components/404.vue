0275153145880da5a3c12ee21350361670c70075
unknown <larrylenw@gmail.com>
2024-08-02 09:32:36 +0700

    init project

---
+++ my-project/src/components/404.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+    <div>
+        <p>Oops, You are lost</p>
+        <RouterLink tag="p" to="/">Go back home</RouterLink>
+    </div>
+</template>
+
+<style></style>
