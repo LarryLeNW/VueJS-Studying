d41d94b647ce4caedcfa721778f105d422402dfe
unknown <larrylenw@gmail.com>
2024-08-03 23:28:46 +0700

    test syntax 3/8

--- vuejs-study/src/main.js
+++ vuejs-study/src/main.js
@@ -1,20 +1,26 @@
-import "./assets/main.css";
-
 import { createApp } from "vue";
-import App from "./App.vue";
-import components from "./syntax/component.vue";
-import emit from "./syntax/emit.vue";
+import testLoading from "./syntax/testLoading.vue";
 import router from "./router";
 import GlobalComponent from "./components/GlobalComponents.vue";
 import { readonly } from "vue";
-const app = createApp(emit);
+import i18n from "./plugins/i18n";
+
+const app = createApp(testLoading);
 
 app.component("ComponentA", GlobalComponent).component(
     "ComponentB",
     GlobalComponent
 );
+
 app.provide("message", readonly("Hello!"));
 
 app.use(router);
+
+app.use(i18n, {
+    greetings: {
+        hello: "Xin chào",
+        message: "Chào mừng bạn đến với khoá học Vue.js",
+    },
+});
 
 app.mount("#app");
